c00b86b43525efd933fb9bd8d7e2d892cfa36de2
Utku Bilge Keskin <utkubilgekeskin@gmail.com>
2022-10-25 18:54:37 +0300

    Initial Commit

---
+++ MyCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCalculator
+//
+//  Created by OS on 25.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
